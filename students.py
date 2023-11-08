e38aba8a925736bb03785b85b83bd11a91c9462f
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-08 11:17:07 -0500

    Activity 10.5

--- students.py
+++ students.py
@@ -1,5 +1,8 @@
 class Student:
-    pass
+    id = 'No id'
+    name = 'Student'
+    credits = 0
+    gpa = 0.0
 
 def print_student(student):
     print(student.id, student.name,student.credits, student.gpa)
@@ -10,13 +13,11 @@ def main():
 
     student_1.id = "1234"
     student_1.name = "Pam"
-    student_1.credits = "14"
-    student_1.gpa = "4.0"
+    student_1.credits = 14
+    student_1.gpa = 4.0
 
     student_2.id = "2000"
     student_2.name = "Jerry"
-    student_2.credits = "10"
-    student_2.gpa = "3.5"
 
     print_student(student_1)
     print_student(student_2)
