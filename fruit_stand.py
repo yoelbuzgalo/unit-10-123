69a3903f0c7d8762bd402647be866b7a072dc801
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-08 11:39:11 -0500

    Activity 10.7

--- fruit_stand.py
+++ fruit_stand.py
@@ -3,7 +3,7 @@ class Fruit:
     price = 1000000
 
 GRAPE = Fruit()
-GRAPE.type = "Grades"
+GRAPE.type = "Grapes"
 GRAPE.price = 3.50
 
 LYCHEE = Fruit()
@@ -14,3 +14,28 @@ PLUM = Fruit()
 PLUM.type = "Plum"
 PLUM.price = 1.75
 
+def add_to_basket(list, fruit):
+    list.append(fruit)
+
+def main():
+    basket = []
+    while True:
+        user_input = input("Enter name of fruit to add to basket: ")
+        if user_input == "":
+            break
+        elif user_input == "Grapes":
+            add_to_basket(basket, GRAPE)
+        elif user_input == "Lychee":
+            add_to_basket(basket, LYCHEE)
+        elif user_input == "Plum":
+            add_to_basket(basket, PLUM)
+        else:
+            raise ValueError("No fruit matched to add for basket")
+        print("Basket: ")
+        for item in basket:
+            print(item.type)
+
+if __name__ == "__main__":
+    main()
+        
+    
