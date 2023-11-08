3ba1fcd471e55e21b773d84bbc8d89a8514a9c69
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-08 11:05:34 -0500

    Activity 10.3

--- students.py
+++ students.py
@@ -5,5 +5,17 @@ def main():
     student_1 = Student()
     student_2 = Student()
 
+    student_1.id = "1234"
+    student_1.name = "Pam"
+    student_1.credits = "14"
+    student_1.gpa = "4.0"
+
+    student_2.id = "2000"
+    student_2.name = "Jerry"
+    student_2.credits = "10"
+    student_2.gpa = "3.5"
+
+    print(student_1.id, student_1.name,student_1.credits, student_1.gpa, student_2.id, student_2.name,student_2.credits, student_2.gpa)
+
 if __name__ == "__main__":
     main()
