74d19cbfe70263c381c0024dfbb935ccc9d8feea
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-08 11:31:40 -0500

    Activity 10.6

---
+++ fruit_stand.py
@@ -0,0 +1,16 @@
+class Fruit:
+    type = ''
+    price = 1000000
+
+GRAPE = Fruit()
+GRAPE.type = "Grades"
+GRAPE.price = 3.50
+
+LYCHEE = Fruit()
+LYCHEE.type = "Lychee"
+LYCHEE.price = 0.50
+
+PLUM = Fruit()
+PLUM.type = "Plum"
+PLUM.price = 1.75
+
