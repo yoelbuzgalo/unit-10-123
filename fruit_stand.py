f417c362380c58862a6dae2c7fe8c95844a0251c
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-10 10:20:42 -0500

    Activity 10.8

--- fruit_stand.py
+++ fruit_stand.py
@@ -17,6 +17,19 @@ PLUM.price = 1.75
 def add_to_basket(list, fruit):
     list.append(fruit)
 
+def price_in_basket(basket):
+    total_price = 0
+    for item in basket:
+        total_price += item.price
+    return total_price
+
+def count_fruits(basket, fruit):
+    count = 0
+    for item in basket:
+        if item == fruit:
+            count += 1
+    return count
+
 def main():
     basket = []
     while True:
@@ -31,9 +44,15 @@ def main():
             add_to_basket(basket, PLUM)
         else:
             raise ValueError("No fruit matched to add for basket")
-        print("Basket: ")
-        for item in basket:
-            print(item.type)
+    
+    grape_count = count_fruits(basket, GRAPE)
+    lychee_count = count_fruits(basket, LYCHEE)
+    plum_count = count_fruits(basket, PLUM)
+    
+    print("Total price of basket: ", price_in_basket(basket))
+    print("You have", grape_count, "bundles of", GRAPE.type)
+    print("You have", plum_count, "bundles of", PLUM.type)
+    print("You have", lychee_count, "bundles of", LYCHEE.type)
 
 if __name__ == "__main__":
     main()
